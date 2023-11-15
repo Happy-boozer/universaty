89517464aadc2f248d88577979d453f8a7d12ac8
Happy_Comrade <62546674+Happy-boozer@users.noreply.github.com>
2023-11-15 21:13:44 +0300

    Update number_5.cpp

--- homeworks/number_5.cpp
+++ homeworks/number_5.cpp
@@ -24,3 +24,31 @@ int main()
 {
     NOD();
 }
+
+void RESHETO() {
+    int n=0, m = 0, d=0;
+    cin >> n;
+    int* arr = new int(n + 3);
+    
+    for (int i = 2; i < n + 3;i++) {
+        arr[i] = i;
+    }
+    //cout << arr[n] << "\n";
+
+    for (int j = 2; j < n; j++) {
+        //cout << arr[j] << "\n";
+        for (int k = 3; k < n++; k++) {
+            //cout << arr[k] << "\n";
+        
+            if (arr[k] % arr[j] == 0 and arr[k]!= arr[j] and arr[k]!=0) {
+                cout << arr[k]<< arr[j] << "\n";
+                arr[k] = 0;
+           }
+        }
+    }
+    //cout << arr << "\n";
+   /*for (int r = 2; r < n+2; r++)
+        cout << arr[r] << "\n";*/
+    return;
+
+}
